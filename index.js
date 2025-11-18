df50ae7e71d2e1ebc8a83a70315c0c229a44e27b
Ashkar S <ashkars178@gmail.com>
2025-11-18 14:16:34 +0530

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer=require('json-server')
+
+const server=jsonServer.create
+const middleWare=jsonServer.defaults
+
+const route=jsonServer.router('db.json')
+
+server.use(middleWare)
+server.use(route)
+
+
+const PORT=process.env.PORT ||3000
+server.listen(PORT,()=>{
+console.log(PORT)
+})
